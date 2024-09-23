5fdac9c5abc70fd5e670cecb30bef6fc950eca0f
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-09-23 21:21:50 +0900

    webs

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,34 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { NEWSData } from './types/types';
-import Newscardlist from './components/newslist/Newscardlist';
+import { NEWSData } from './types/types'
+import Newscardlist from './components/newslist/Newscardlist'
+import prisma from '../lib/prismaClient'
 
 async function getNewsData() {
-  const response = await fetch("http://localhost:3000/api/news", {
-    cache: "no-store",
-  }); 
-
-  const newsItems: NEWSData = await response.json();
-
-  return newsItems;
+  const newsData = await prisma.news.findMany()
+  return newsData
 }
 
 export default async function Home() {
-  const newsData = await getNewsData();
+  const newsData = await getNewsData()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24">
       {/* News Section */}
       <section id="news" className="w-full mt-24">
         <h2 className="text-4xl font-bold ml-[10%] filter drop-shadow-lg">News</h2>
-        <Newscardlist newsData={[newsData]}/>
+        <Newscardlist newsData={newsData}/>
         <div className="text-right mr-[10%] mt-8">
           <Link href="/news" className="text-xl font-semibold hover:text-blue-500 transition-colors">See more→</Link>
         </div>
       </section>
       
-        {/* About Section */}
       {/* Lab Introduction */}
       <section id="labo" className="w-full mt-24 relative">
         <h2 className="text-4xl font-bold ml-[10%] filter drop-shadow-lg">LABO</h2>
         <div className="w-1/2 ml-[200px] mt-8 mb-8 mr-[600px] overflow-y-auto h-[40vh]">
           深層学習を利用した豚肺疾患の検出及び鑑別に関する研究や、パノラマビジョンシステムを利用した胸腔鏡手術支援に関する研究、パターン認識技術を駆使したアプリ制作などを行っています
         </div>
-        {/* <div className="text-center my-6">
-          <a href="https://www.youtube.com/embed/0u01tlNkWnA?si=BoVtTSs7vOpIjzBR" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">紹介動画</a>
-        </div>
-        <div className="flex overflow-x-auto space-x-6 my-6">
-          {[1, 2, 3, 4, 5].map((num) => (
-            <div key={num} className="flex-none w-[450px]">
-              <Image src={`/images/studyimg${num}.jpg`} alt="" width={450} height={300} objectFit="cover" />
-            </div>
-          ))}
-        </div>
-        <div className="text-right mr-[10%] mt-8">
-          <Link href="/gallery" className="text-xl font-semibold hover:text-blue-500 transition-colors">See more→</Link>
-        </div> */}
         <div className="absolute top-0 left-0 w-3/5 h-[40vh] bg-gray-200 bg-opacity-80 -z-10"></div>
       </section>
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    env: {
-        DATABASE_URL: process.env.DATABASE_URL,
-      },
-      // Prismaを使用する場合、以下の設定を追加
-      experimental: {
-        serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-      },
-};
-
-
-export default nextConfig;
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    experimental: {
+      serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+    },
+  };
+  
+  export default nextConfig;
