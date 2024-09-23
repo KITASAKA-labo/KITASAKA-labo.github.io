686dba1f430dbf573027768e625fdf4c0a96f650
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-09-23 16:02:41 +0900

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
+      // Prismaを使用する場合、以下の設定を追加
+      experimental: {
+        serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+      },
 };
+
 
 export default nextConfig;
