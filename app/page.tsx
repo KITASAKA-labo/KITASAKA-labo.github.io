e39af77593187dbb5ec3c132c5cf6a3d96134cc4
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-09-26 10:51:54 +0900

    up-to-date

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24">
 
-      <div id="bodyTitle" className="relative z-10 flex flex-col items-center justify-center h-full text-white">
+      <div id="bodyTitle" className="relative z-10 flex flex-col items-center justify-center h-screen text-white z-2">
         <div className="Title text-center mb-8">
           <h1 className="text-4xl md:text-6xl font-bold mb-2">北坂研究室</h1>
-          <h3 className="text-xl md:text-2xl">Medical Image Processing Laboratory</h3>
+          <h3 className="text-xl md:text-2xl ">Medical Image Processing Laboratory</h3>
         </div>
-        <div id="top1" className="absolute top-0 left-0 w-screen h-full z--2">
+        <div id="top1" className="absolute top-0 left-0 w-screen h-screen z-1">
           <Image
             src={"https://github.com/KITASAKA-labo/KITASAKA-labo.github.io/blob/main/app/images/top/topimg1.png?raw=true"}
             alt="Top image 1"
