e3fbf35398069acac6ec8c9d6c7413a3e6064005
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-01 13:49:37 +0900

    up-to-date

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -21,17 +21,6 @@ export default async function Home() {
         <Gallerylist galleryData={galleryData}/>
       </div>
 
-      {/* Footer */}
-      <footer className="w-full mt-36 pt-8 border-t border-gray-300 text-center">
-        <div className="mb-24">
-          <nav className="space-y-4">
-            <Link href="/" className="block hover:text-blue-500 transition-colors">HOME</Link>
-            <Link href="/gallery" className="block hover:text-blue-500 transition-colors">研究・制作</Link>
-            <Link href="/contact" className="block hover:text-blue-500 transition-colors">お問い合わせ</Link>
-          </nav>
-        </div>
-        <p className="text-sm text-gray-600">© 2024 医用画像処理研究室.</p>
-      </footer>
     </main>
   )
 }
