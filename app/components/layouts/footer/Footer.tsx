d079cd889e5e7890cfd948c1b5367c43d8c2291f
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-01 13:45:45 +0900

    up-to-date

---
+++ app/components/layouts/footer/Footer.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React, { useState, useRef } from 'react'
+import Link from 'next/link'
+
+export default function Component() {
+  
+
+  return (
+    <footer className="w-full mt-36 pt-8 border-t border-gray-300 text-center">
+        <div className="mb-24">
+          <nav className="space-y-4">
+            <Link href="../../index.html" className="block hover:text-blue-500 transition-colors">HOME</Link>
+            <Link href="../../../Gallery" className="block hover:text-blue-500 transition-colors">研究・制作</Link>
+            <Link href="/contact" className="block hover:text-blue-500 transition-colors">お問い合わせ</Link>
+          </nav>
+        </div>
+        <p className="text-sm text-gray-600">© 2024 医用画像処理研究室.</p>
+      </footer>
+  )
+}
--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -50,16 +50,15 @@ export default function Component() {
                   <div className="absolute left-0 top-full mt-1 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5">
                     <Link href="../../Gallery" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">研究一覧</Link>
                     <Link href="./gararry.html?cat=production" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">制作一覧</Link>
-                    <Link href="./news.html" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">活動情報</Link>
-                    <Link href="work.html" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">成果一覧</Link>
+                    <Link href="../../News" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">活動情報</Link>
+                    <Link href="../../Work" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">成果一覧</Link>
                   </div>
                 )}
               </div>
               <div className="relative flex items-center h-full">
                 <Link
                   href="https://fpms.aitech.ac.jp/Main.php?action=profile&type=detail&tchCd=4d7a67784e7a63324d4441774d412e2e&pre_action=&pre_andor=&andor=&pre_key1=&key1=&pre_key2=&key2=&pre_key3=&key3=&pre_Facultyk=&Facultyk=&pre_kkb_c=&kkb_c=&pre_kbnCode=&kbnCode=&pre_fName=&fName=&pre_gName=&gName=&pre_FacultyVal=&FacultyVal=&pre_DepartmentVal=&DepartmentVal=&pre_job=&job=&pre_Society=&Society=&pre_FieldKeyword=&FieldKeyword=&pre_gsk_author=&gsk_author=&pre_gsk_title=&gsk_title=&pre_offset=&offset=&pre_cntno=&cntno=&searched="
-                  className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 transition duration-150 ease-in-out h-full"
-                >
+                  className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 transition duration-150 ease-in-out h-full">
                   FACULTY
                 </Link>
               </div>
@@ -78,7 +77,7 @@ export default function Component() {
                   <div className="absolute right-0 top-full mt-1 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5">
                     <Link href="https://www.ait.ac.jp/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">学校情報</Link>
                     <Link href="https://www.ait.ac.jp/faculty/info-science/index.html#headline-1421584671" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">学部情報</Link>
-                    <Link href="./labo.html" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">研究室情報</Link>
+                    <Link href="../../Labo" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">研究室情報</Link>
                   </div>
                 )}
               </div>
