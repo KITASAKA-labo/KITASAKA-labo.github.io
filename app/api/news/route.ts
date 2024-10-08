5b2ff683f1954588cdc59cf2189814ee6a0f756e
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-08 17:26:30 +0900

    up-to-date

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
-import prisma from '../../../lib/prismaClient'
+// import { NextResponse } from 'next/server';
+// import prisma from '../../../lib/prismaClient'
 
-export async function GET(req: Request){
-    const allPosts = await prisma.news.findMany();
-    return NextResponse.json(allPosts);
-};
+// export async function GET(req: Request){
+//     const allPosts = await prisma.news.findMany();
+//     return NextResponse.json(allPosts);
+// };
