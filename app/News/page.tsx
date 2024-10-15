70d844b990ef9200e6ef911f903fdc365b37a8d0
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-15 15:10:16 +0900

    up-to-date

--- app/News/page.tsx
+++ app/News/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { GALLERYData } from '../types/types'
-import Newscardlist from '../components/newslist/Newscardlist'
+import { NEWSData } from '../types/types'
+import NewsGcardlist from '../components/newsgallery/NewsGcardlist'
 import prisma from '../../lib/prismaClient'
 
 async function getNewsData() {
@@ -9,29 +9,28 @@ async function getNewsData() {
   return newsData
 }
 
+
 export default async function Home() {
   const newsData = await getNewsData()
-
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24">
-      
-      <section id="news" className="w-full mt-24">
-        <h2 className="text-4xl font-bold ml-[10%] filter drop-shadow-lg">News</h2>
-        <Newscardlist newsData={newsData}/>
-        <div className="text-right mr-[10%] mt-8">
-          <Link href="/news" className="text-xl font-semibold hover:text-blue-500 transition-colors">See more→</Link>
+
+      <div id="bodyTitle" className="relative h-screen flex items-center justify-center">
+        <div className="Title text-center text-white z-10">
+          <h1 className="text-6xl font-bold mb-4">北坂研究室</h1>
+          <h3 className="text-3xl">Medical Image Processing Laboratory</h3>
         </div>
-      </section>
-      
-      
-      <section id="labo" className="w-full mt-24 relative">
-        <h2 className="text-4xl font-bold ml-[10%] filter drop-shadow-lg">LABO</h2>
-        <div className="w-1/2 ml-[200px] mt-8 mb-8 mr-[600px] overflow-y-auto h-[40vh]">
-          深層学習を利用した豚肺疾患の検出及び鑑別に関する研究や、パノラマビジョンシステムを利用した胸腔鏡手術支援に関する研究、パターン認識技術を駆使したアプリ制作などを行っています
-        </div>
-        <div className="absolute top-0 left-0 w-3/5 h-[40vh] bg-gray-200 bg-opacity-80 -z-10"></div>
-      </section>
-      
+        <div id="bgmovie-mask" className="absolute inset-0 bg-black bg-opacity-30" />
+      </div>
+
+      <div id="news_g" className="container mx-auto mt-20 px-4">
+        <h2 className="text-4xl font-bold mb-10">News</h2>
+        <ul id="news_garrary" className="space-y-6">
+          <NewsGcardlist newsData={newsData}/>
+        </ul>
+      </div>
+
     </main>
   )
 }
