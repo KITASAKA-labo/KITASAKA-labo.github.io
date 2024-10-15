df2577eab19b5b867b5d88c32eac5ced989dc634
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-15 15:14:31 +0900

    up-to-date

--- app/News/page.tsx
+++ app/News/page.tsx
@@ -21,12 +21,20 @@ export default async function Home() {
           <h1 className="text-6xl font-bold mb-4">北坂研究室</h1>
           <h3 className="text-3xl">Medical Image Processing Laboratory</h3>
         </div>
-        <div id="bgmovie-mask" className="absolute inset-0 bg-black bg-opacity-30" />
+        <div id="top1" className="absolute top-0 left-0 w-screen h-screen z-1">
+          <Image
+            src={"https://github.com/KITASAKA-labo/KITASAKA-labo.github.io/blob/main/public/images/top/topimg1.png?raw=true"}
+            alt="Top image 1"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+        </div>
       </div>
 
       <div id="news_g" className="container mx-auto mt-20 px-4">
         <h2 className="text-4xl font-bold mb-10">News</h2>
-        <ul id="news_garrary" className="space-y-6">
+        <ul id="news_garrary" className="w-[100%] space-y-6 item-center">
           <NewsGcardlist newsData={newsData}/>
         </ul>
       </div>
