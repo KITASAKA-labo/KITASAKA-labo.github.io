3881b85394e4fc1bb022c3d498e1719f8a0de89e
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-15 15:32:19 +0900

    up-to-date

--- app/components/newsgallery/NewsGallery.tsx
+++ app/components/newsgallery/NewsGallery.tsx
@@ -12,7 +12,7 @@ export const NewsGallery = ({newsItems}: NewsCardProps) => {
   const {day, news, img, url} = newsItems;
 
   return (
-    <li className="newsitem_g border-t border-gray-200 pt-4">
+    <li className="newsitem_g border-t border-gray-200 pt-4 w-[100%]">
         <Link href={`${url}`} className="flex flex-col md:flex-row justify-between items-start text-black no-underline">
             <div className="newsday_g font-semibold mb-2 md:mb-0 md:w-1/4">{day}</div>
             <div className="newscontent_g md:w-3/4">{news}</div>
