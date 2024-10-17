ff74428ee4ee9f4414a411352f1b9cf5d60e6d90
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-17 14:25:06 +0900

    up-to-date

--- app/components/layouts/footer/Footer.tsx
+++ app/components/layouts/footer/Footer.tsx
@@ -12,7 +12,7 @@ export default function Component() {
           <nav className="space-y-4">
             <Link href="../../index.html" className="block hover:text-blue-500 transition-colors">HOME</Link>
             <Link href="../../../Gallery" className="block hover:text-blue-500 transition-colors">研究・制作</Link>
-            <Link href="/contact" className="block hover:text-blue-500 transition-colors">お問い合わせ</Link>
+            <Link href="../../../Labo" className="block hover:text-blue-500 transition-colors">お問い合わせ</Link>
           </nav>
         </div>
         <p className="text-sm text-gray-600">© 2024 医用画像処理研究室.</p>
