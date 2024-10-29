d23d67bb704ee0064dba1768b6bd715e84faae41
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-29 10:51:13 +0900

    up-to-date

--- app/components/slider/InfiniteSlider.tsx
+++ app/components/slider/InfiniteSlider.tsx
@@ -23,11 +23,11 @@ const InfiniteSlider = () => {
   const settings = {
     dots: false,
     infinite: true,
-    speed: 1000,
+    speed: 1500,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 100,
     responsive: [
       {
         breakpoint: 1024,
