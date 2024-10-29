e78c1d9a81851995df6fbed3e1b394354584a8cb
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-10-29 11:19:52 +0900

    up-to-date

--- app/components/layouts/footer/Footer.tsx
+++ app/components/layouts/footer/Footer.tsx
@@ -10,9 +10,9 @@ export default function Component() {
     <footer className="w-full mt-36 pt-8 border-t border-gray-300 text-center">
         <div className="mb-24">
           <nav className="space-y-4">
-            <Link href="../../index.html" className="block hover:text-blue-500 transition-colors">HOME</Link>
-            <Link href="../../../Gallery" className="block hover:text-blue-500 transition-colors">研究・制作</Link>
-            <Link href="../../../Labo" className="block hover:text-blue-500 transition-colors">お問い合わせ</Link>
+            <Link href="../../index.html" className="block hover:text-blue-500 transition-colors text-sm">HOME</Link>
+            <Link href="../../../Gallery" className="block hover:text-blue-500 transition-colors text-sm">研究・制作</Link>
+            <Link href="../../../Labo" className="block hover:text-blue-500 transition-colors text-sm">お問い合わせ</Link>
           </nav>
         </div>
         <p className="text-sm text-gray-600">© 2024 医用画像処理研究室.</p>
